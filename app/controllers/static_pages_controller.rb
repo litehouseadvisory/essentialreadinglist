e08b1579e04aecd74200e9e9601d9fe0353d1a07
litehouseadvisory <marc.vaneecke@litehouseadvisory.com>
2013-08-27 14:54:00 +0200

    finish static

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def intro
+  end
+
+  def home
+  end
+
+  def help
+  end
+  
+  def about
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Gwerl::Application.routes.draw do
+  get "static_pages/intro"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
